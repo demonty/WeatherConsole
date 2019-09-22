9a1a21f2d0c41befa9fb960bf9bbd8e968d02abe
demonty <47073457+demonty@users.noreply.github.com>
2019-09-22 02:49:13 -0500

    basic functionality runs as expected

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -2,17 +2,21 @@
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.IO;
+using System.Configuration;
+
 
 namespace WeatherApp
 {
     public class Options
     {
-        public int zip { get; set; }
-        public int units { get; set; }
-        public int type { get; set; }
+        public string zip { get; set; }
+        public string units { get; set; }
+        public string type { get; set; }
         public bool IsZipCode(string zipCode)
         {
             var _usZipCode = @"\d{5}$";
@@ -28,17 +32,19 @@ namespace WeatherApp
     }
     class Program
     {
-        static HttpClient http = new HttpClient();
+        static readonly HttpClient http = new HttpClient();
 
         static void ShowWeather(WeatherCurrentMessage weather)
         {
+            Console.Clear();
             Console.WriteLine($"Name: {weather.name}");
         }
 
         static async Task<WeatherCurrentMessage> GetWeatherAsync(string path)
         {
             WeatherCurrentMessage weather = null;
-            HttpResponseMessage response = await http.GetAsync(path);
+            var uri = new Uri(path);
+            HttpResponseMessage response = await http.GetAsync(uri.AbsoluteUri);
 
             if (response.IsSuccessStatusCode)
             {
@@ -50,17 +56,140 @@ namespace WeatherApp
 
         static async Task RunAsync()
         {
-            WeatherCurrentMessage msg = new WeatherCurrentMessage();
-            msg = await GetWeatherAsync("");
+            Options opt = RunMenu();
+
+            string url = GetRequestURL(opt);
+            Console.WriteLine($"url: {url}");
+
+            try
+            {
+
+                WeatherCurrentMessage msg = await GetWeatherAsync(url);
+                ShowWeather(msg);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         static string GetRequestURL(Options opt)
         {
-            return "";
+            string url = "https://api.openweathermap.org/data/2.5/";
+            url += opt.type;
+            url += "zip=" + opt.zip;
+            
+            if(opt.units != "Standard")
+            {
+                url += "&units=" + opt.units;
+            }
+
+            url += "&APPID=" + ConfigurationManager.AppSettings["APIKey"];
+            return url;
         }
 
         static void Main(string[] args)
         {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static Options RunMenu()
+        {
+            Options opt = new Options();
+            var menu = true;
+            string input;
+
+            while (menu)
+            {
+                Console.Clear();
+                Console.WriteLine("Please Enter Zipcode: ");
+                Console.WriteLine("---------------");
+                Console.WriteLine();
+                input = Console.ReadLine();
+                if (opt.IsZipCode(input))
+                {
+                    opt.zip = input;
+                    menu = false;
+                }
+                else
+                {
+                    Console.WriteLine("INVALID ZIPCODE");
+                    Thread.Sleep(2000);
+                }
+            }
+
+            menu = true;
+
+            while (menu)
+            {
+                Console.Clear();
+                Console.WriteLine("Weather Query: ");
+                Console.WriteLine("---------------");
+                Console.WriteLine();
+                Console.WriteLine("1: Current Weather");
+                Console.WriteLine("2: Weather Forecast");
+                Console.WriteLine("---------------");
+                Console.WriteLine();
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        opt.type = "weather?";
+                        menu = false;
+                        break;
+                    case "2":
+                        opt.type = "forecast?";
+                        menu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Selection. Please try again.");
+                        Thread.Sleep(2000);
+                        break;
+                }
+            }
+
+            menu = true;
+
+            while (menu)
+            {
+                Console.Clear();
+                Console.WriteLine("Weather Units: ");
+                Console.WriteLine("---------------");
+                Console.WriteLine();
+                Console.WriteLine("1: Imperial");
+                Console.WriteLine("2: Metric");
+                Console.WriteLine("3: Standard");
+                Console.WriteLine("---------------");
+                Console.WriteLine();
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        opt.units = "Imperial";
+                        menu = false;
+                        break;
+                    case "2":
+                        opt.units = "Metric";
+                        menu = false;
+                        break;
+                    case "3":
+                        opt.units = "";
+                        menu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Selection. Please try again.");
+                        Thread.Sleep(2000);
+                        break;
+                }
+            }
+
+            Console.Clear();
+
+            return opt;
         }
     }
 }
